6512e7357fc9767b5b4f9f5dc31f518e6b4dbe6d
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2024-01-08 09:37:35 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -10,6 +10,7 @@ from models.base_model import BaseModel, Base
 class Amenity(BaseModel, Base):
     """Represents the amenity data set."""
     __tablename__ = 'amenities'
+    __table_args__ = {'extend_existing':True}
     name = Column(
         String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
