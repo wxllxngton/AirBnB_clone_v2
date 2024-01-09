9ec09d1956ffedb36647e3c784c412e9110ea8b1
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2024-01-09 16:15:26 +0300

    Update 100-clean_web_static.py: pycodestyle

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -57,7 +57,10 @@ def do_deploy(archive_path):
         run("rm /tmp/{}".format(archive))
 
         # Move the contents of web_static to the release directory
-        run("mv {}/{}/web_static/* {}/{}/".format(path, folder[0], path, folder[0]))
+        run("mv {}/{}/web_static/* {}/{}/".format(path,
+                                                  folder[0],
+                                                  path,
+                                                  folder[0]))
 
         # Remove the web_static directory in the release directory
         run("rm -rf {}/{}/web_static".format(path, folder[0]))
@@ -90,17 +93,20 @@ def do_clean(number=0):
     if number == 0 or number == 1:
         local("ls -t versions/ | tail -n +2 | xargs -I {} rm versions/{}")
         run(
-            "ls -t /data/web_static/releases | tail -n +2 | xargs -I {} rm -rf /data/web_static/releases/{}"
+            "ls -t /data/web_static/releases | tail -n +2 |\
+            xargs -I {} rm -rf /data/web_static/releases/{}"
         )
 
     elif number > 1:
         local(
-            "ls -t versions/ | tail -n +{} | xargs -I {} rm versions/{}".format(
+            "ls -t versions/ | tail -n +{} | xargs -I {} rm\
+            versions/{}".format(
                 number + 1
             )
         )
         run(
-            "ls -t /data/web_static/releases | tail -n +{} | xargs -I {} rm -rf /data/web_static/releases/{}".format(
+            "ls -t /data/web_static/releases | tail -n +{} |\
+            xargs -I {} rm -rf /data/web_static/releases/{}".format(
                 number + 1
             )
         )
