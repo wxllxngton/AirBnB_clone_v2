30196dacae0f9e2b9a1063b4788cbf7be55cb91d
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2024-01-09 16:20:52 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,8 @@ import os
 from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 
-storage = DBStorage() if os.getenv("HBNB_TYPE_STORAGE") == "db" else FileStorage()
+storage = DBStorage() if os.getenv("HBNB_TYPE_STORAGE") == "db"\
+else FileStorage()
 """A unique switch between FileStorage/DBStorage instance for all models.
 """
 storage.reload()
